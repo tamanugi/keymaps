cf3540e41b61cdbf1ca8474d694679a789260f22
tamanugi <tamanugi@gmail.com>
2019-12-20 11:32:31 +0900

    👍 GUIを２つに増やした

--- ergodash/keymap.c
+++ ergodash/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Space|                    | Enter|   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | `    |      |  ALT |      |  GUI | Lower| Space| Space|||||||| Enter| Enter| Raise|      | Left | Down |  Up  | Right|
+   * | `    |  ALT |  GUI |      |  GUI | Lower| Space| Space|||||||| Enter| Enter| Raise|      | Left | Down |  Up  | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-     KC_GRV, KC_LALT, KC_LALT,KC_LGUI, LOWER,    KC_SPC , KC_SPC,         KC_ENT,KC_BSPC, RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+     KC_GRV, KC_LALT, KC_LGUI,KC_LGUI, LOWER,    KC_SPC , KC_SPC,         KC_ENT,KC_BSPC, RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
   /* Lower
